a108788193076c0e7d95a770e8915bdd2b36e209
lorevir <lviprieto@gmail.com>
2025-05-22 14:31:55 -0300

    Se incorpora información de Misiones

--- ui.R
+++ ui.R
@@ -15,7 +15,7 @@ shinyUI(
              
              tabPanel("RESUMEN",
                       
-                      tags$head(includeHTML("/srv/DataDNMYE/login_shiny/areas-protegidas.html")),
+                      #tags$head(includeHTML("/srv/DataDNMYE/login_shiny/areas-protegidas.html")),
                       
                       useWaiter(),
                       waiter_show_on_load(html = loading_screen, color = "white"),
@@ -180,10 +180,11 @@ shinyUI(
                       div(id="container-info",
                       h5(tags$p("Los datos presentados en este tablero surgen de los registros administrativos de la Dirección 
                                          Nacional de Uso Público de la Administración de Parques Nacionales, del Departamento Observatorio
-                                         Turístico del Chubut y del Parque Provincial Isghigualasto, encargados de recopilar y procesar
+                                         Turístico del Chubut, del Parque Provincial Isghigualasto, la Dirección de Parques de la Provincia de Corrientes, Intendencia Parque Iberá y la Dirección de Mercados y Estadística del 
+                                         Ministerio de Turismo de Misiones, encargados de recopilar y procesar
                                          los datos de visitas.")),
                       h5(tags$p("La información suministrada permite la clasificación de las visitas en residentes y 
-                                         no residentes en 42 Parques Nacionales, en 6 Áreas Naturales Protegidas del Chubut, 1 Parque Provincial en San Juan y 1 Parque en Corrientes. En enero 2024 se incorporó el registro de visitas en el PN Ansenuza y en julio 2024 el PN Islas de Santa Fe.")),
+                                         no residentes en 42 Parques Nacionales, en 6 Áreas Naturales Protegidas del Chubut, 1 Parque Provincial en San Juan, 1 Parque en Corrientes y 5 Parques Provinciales en Misiones. En enero 2024 se incorporó el registro de visitas en el PN Ansenuza y en julio 2024 el PN Islas de Santa Fe.")),
                       
                       
                           br(),
@@ -193,10 +194,10 @@ shinyUI(
                           tags$ul(
                             tags$li(style = "color: black", tags$b("Visita:"), " entrada a un parque nacional con cualquier finalidad principal (ocio,negocios u otro motivo personal) y que no deba ser empleado por el parque nacional (cada vez que se cruza la frontera del área protegida, se genera una visita."),br(),
                             tags$li(style = "color: black", tags$b("Unidades de observación:"), "Visitantes."),br(),
-                            tags$li(style = "color: black", tags$b("Unidades de análisis:"), "Nacionales: 42 áreas protegidas que producen información estadística de un total de 55 áreas nacionales (Parques Nacionales, Monumentos Naturales y Reservas Nacionales, Reservas Naturales, Reserva Natural Estricta y Reserva Natural Educativa). Provinciales: 5 Áreas Naturales Protegidas del Chubut, 1 Parque Provincial de San Juan."),br(),
+                            tags$li(style = "color: black", tags$b("Unidades de análisis:"), "Nacionales: 42 áreas protegidas que producen información estadística de un total de 55 áreas nacionales (Parques Nacionales, Monumentos Naturales y Reservas Nacionales, Reservas Naturales, Reserva Natural Estricta y Reserva Natural Educativa). Provinciales: 5 Áreas Naturales Protegidas del Chubut, 1 Parque Provincial de San Juan, 1 Provincial en Corrientes y 5 Parques Provinciales en Misiones."),br(),
                             tags$li(style = "color: black", tags$b("Forma de colecta:"), "Las áreas protegidas contabilizan las visitas en base a la venta de boletos o al registro de visitantes en los diferentes portales de ingresos (pueden presentar más de un portal de acceso."),br(),     
                             tags$li(style = "color: black", tags$b("Período de referencia del dato:"), "Mensual."),br(), 
-                            tags$li(style = "color: black", tags$b("Variables de estudio:"), "Cantidad de visitas realizadas de cada área protegida y condición de residencia en Áreas Protegidas Nacionales, del Chubut y San Juan; total de visitas en el Gran Parque Iberá."),br(),
+                            tags$li(style = "color: black", tags$b("Variables de estudio:"), "Cantidad de visitas realizadas de cada área protegida y condición de residencia en Áreas Protegidas Nacionales, del Chubut y San Juan; total de visitas en el Gran Parque Iberá, Parque Provincial Moconá, Parque Provincial Salto Encantado, Reducciones Jesuíticas San Ignacio, Reducciones Jesuíticas Santa Ana y el Espectáculo de Imagen y Sonido."),br(),
                             tags$li(style = "color: black", tags$b("Cobertura geográfica de las Áreas protegidas nacionales:"), "6 regiones turísticas compuestas por los siguientes Parques Nacionales:"),
                             tags$b("1. Región Buenos Aires:"), "Ciervo de los Pantanos.",br(),
                             tags$b("2. Región Córdoba:"), "Quebrada del Condorito, Traslasierra.",br(),
@@ -206,10 +207,11 @@ shinyUI(
                             tags$b("6. Región Patagonia:"), "Los Glaciares, Nahuel Huapi, Tierra del Fuego, Los Alerces,Lago Puelo, Lanín, Laguna Blanca, Lihué Calel, Monte León, Perito Moreno, Bosques Petrificados, Los Arrayanes, Isla Pingüino, Patagonia.",br(),br(),
                             tags$li(style = "color: black", tags$b("Cobertura geográfica de las Áreas protegidas provinciales:"), "3 regiones turísticas compuestas por las siguientes áreas protegidas:"),
                             tags$b("3. Región Cuyo:"), "Ischigualasto.",br(),
-                            tags$b("3. Región Litoral:"), "Gran Parque Iberá.",br(),
+                            tags$b("4. Región Litoral:"), "Gran Parque Iberá (Prov. Corrientes).",br(),
+                            tags$b("4. Región Litoral:"), "Parque Provincial Moconá, Parque Provincial Salto Emcantado, Reducciones Jesuíticas San Ignacio, Reducciones Jesuíticas Santa Ana, Espectáculo de Imagen y Sonido (Prov. Misiones).",br(),
                             tags$b("6. Región Patagonia:"), "Penísula Valdés, Punta Marqués, Bosque Petrificado Sarmiento, Punta Loma, Punta Tombo.",br(),br(),
                             tags$li(style = "color: black", tags$b("Cobertura temporal - Áreas Protegidas Nacionales:"),"a partir del año 2008."),br(),
-                            tags$li(style = "color: black", tags$b("Cobertura temporal - Áreas Protegidas Provinciales:"),"Chubut: a partir del año 2000 (excepto Punta Marques, a partir del 2012). San Juan: a partir del año 2000. Corrientes: a partir del año 2015."),br(),
+                            tags$li(style = "color: black", tags$b("Cobertura temporal - Áreas Protegidas Provinciales:"),"Chubut: a partir del año 2000 (excepto Punta Marques, a partir del 2012). San Juan: a partir del año 2000. Corrientes: a partir del año 2015. Misiones: a partir del año 2015 (excepto el Parque Provincial Moconá, a partir del 2016)"),br(),
                             tags$li(style = "color: black", tags$b("*Notas:"),"A partir del 2021, se pueden descargar las notas que se presentan en los informes mensuales de visitas en áreas protegidas en un archivo .xlsx en la solapa", tags$b("Tablero"), ". Refieren a una serie de eventos o situaciones particulares que impactaron en el movimiento de visitas al área protegida o incidieron sobre el registro de las mismas.")
                             ),
                           
@@ -220,12 +222,13 @@ shinyUI(
                             tags$li(style = "color: black", tags$b("SIAPN:"), "Sistema de Administración de Parques Nacionales"), 
                             tags$li(style = "color: black", tags$b("Chubut:"), "Departamento Observatorio Turístico del Chubut"), 
                             tags$li(style = "color: black", tags$b("San Juan:"), "Parque Provincial Ischigualasto"),
-                            tags$li(style = "color: black", tags$b("Corrientes:"), "Departamento Técnico del Comité Iberá")),
+                            tags$li(style = "color: black", tags$b("Corrientes:"), "Departamento Técnico del Comité Iberá"),
+                            tags$li(style = "color: black", tags$b("Misiones:"), "Dirección de Mercados y Estadística del Ministerio de Turismo de Misiones")),
                       br()
                           
                       
                       )
-             ),  footer = includeHTML("/srv/shiny-server/recursos/shiny_footer.html") #descomentariar al pushear
+             )#,  footer = includeHTML("/srv/shiny-server/recursos/shiny_footer.html") #descomentariar al pushear
   )
 )
 
